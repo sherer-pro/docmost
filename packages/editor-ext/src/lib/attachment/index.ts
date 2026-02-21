e2f3004123cf768c03cacc500da4b26a386f1f2e
Pavel Sherer <pavel@sherer.pro>
2026-02-21 17:17:36 +0300

    feat(editor-ext): add attachment display mode attribute

--- packages/editor-ext/src/lib/attachment/index.ts
+++ packages/editor-ext/src/lib/attachment/index.ts
@@ -1,2 +1,7 @@
 export { Attachment } from "./attachment";
+export type {
+  AttachmentAttributes,
+  AttachmentDisplayMode,
+  AttachmentOptions,
+} from "./attachment";
 export * from "./attachment-upload";
